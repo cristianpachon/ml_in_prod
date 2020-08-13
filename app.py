9a3f4b9e19a3abf73c88c6c0efa0dfd9b65ed2fb
cristianpachon <cc.pachon@gmail.com>
2020-08-13 21:37:15 +0200

    Managing errors in FE part

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from forms import InputFeatures
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '5ea24f645f63b6f9316f899c8da889cd'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = InputFeatures()
-    return render_template('index.html', form=form)
+    if form.validate_on_submit():
+        return redirect(url_for('display_name'))
+    else:
+        return render_template('index.html', form=form)
 
-
-@app.route('/your_name', methods = ['POST'])
-def display_name(name=None):
-    form = InputFeatures(request.form)
-    name = form.username.data
-    if name is None:
+@app.route('/display_name', methods = ['POST'])
+def display_name():
+    try:
+        name = request.form.get('username')
+    except:
         name = 'Loser'
     return render_template('display_name.html', name=name)
 
--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import DecimalField, StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 class InputFeatures(FlaskForm):
-    username = StringField('Name')
+    username = StringField('Name', validators = [DataRequired(), Length(min=7)])
     usersurname = StringField('Surname')
     submit = SubmitField('Submit')
