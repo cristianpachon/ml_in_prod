87ba9e97fa06a52b446a7c7ec058e0e868d56146
cristianpachon <cc.pachon@gmail.com>
2020-09-02 16:55:14 +0200

    Adding logging module

--- form/routes.py
+++ form/routes.py
@@ -2,6 +2,15 @@ from form import app
 from form.forms import InputFeatures
 from flask import Flask, render_template, redirect, url_for, session
 from model.get_prediction import get_prediction
+import logging
+import logging.config
+import yaml
+
+# Logging initialisation
+with open('logging.yaml', 'r') as f:
+    log_cfg = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_cfg)
+    logger = logging.getLogger('simpleExample')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -10,10 +19,12 @@ def index():
         session['median_age'] = form.median_age.data
         return redirect(url_for('display_info'))
     else:
+        logger.info('Rendenring index')
         return render_template('index.html', form=form)
 
 @app.route('/display_info', methods = ['GET', 'POST'])
 def display_info():
+    logger.info('Displaying prediction')
     median_age = session['median_age']
     features = {'median_age':median_age}
     price = get_prediction(features)[0][0]
