6749671134b359256505bb0cd2e7fc2cf14b61e1
cristianpachon <cc.pachon@gmail.com>
2020-09-07 18:21:52 +0200

    improving tests

--- tests/tests.py
+++ tests/tests.py
@@ -6,10 +6,10 @@ def test_model():
     features = {'median_age' : 12}
     
     try:
-        predicted_value = get_prediction(features)
+        predicted_value = get_prediction(features)[0][0]
     except:
         predicted_value = 0
     
-    real_value = 191392.54
+    real_value = 191392.543992269
     diff = abs(predicted_value - real_value)
     assert diff < 1e-6
